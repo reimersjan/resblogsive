480c7698b388766211bdb5dd69d363520e1a3067
Jan Reimers <me@janreimers.net>
2013-11-12 12:53:49 +0100

    cleanup

--- single.php
+++ single.php
@@ -5,15 +5,12 @@
 		<?php while (have_posts()) : the_post(); ?>
 		
 			<article class="post" id="post-<?php the_ID(); ?>">
-			
 				<h2><?php the_title(); ?></h2>
-				
 				<?php the_content('Read Full Article'); ?>
 				
 				<p class="meta">
 					<?php the_time('F jS, Y'); ?> - <?php the_author(); ?>
 				</p>
-				
 			</article>
 			
 		<?php endwhile; ?>
