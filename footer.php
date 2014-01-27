1ae0231dcf3c81a8821eabac4fa567f0f822f06b
Jan Reimers <me@janreimers.net>
2014-01-27 14:49:27 +0100

    footer class

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 </div> <!-- .wrapper -->
 	
 
-<footer>
+<footer class="footer">
 	<p>powered by <a target="_blank" href="http://wordpress.org">WordPress</a> and <a target="_blank" href="<?php echo wp_get_theme()->get( 'ThemeURI' ); ?>"><?php echo wp_get_theme()->get( 'Name' ); ?></a> <?php echo wp_get_theme()->get( 'Version' ); ?></p>
 </footer>
 
