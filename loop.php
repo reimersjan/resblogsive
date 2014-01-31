10504d98ef89dbe27c7c98642c813b55da6bf66f
Jan Reimers <me@janreimers.net>
2014-01-31 22:37:38 +0100

    cleanup + datetime

--- loop.php
+++ loop.php
@@ -1,22 +1,13 @@
 <?php if (have_posts()) : ?>
-
 	<?php while (have_posts()) : the_post(); ?>
-	
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		
 			<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permalink to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 			<p class="meta">
-				<time><?php the_time('F jS, Y'); ?></time>
+				<time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time('F jS, Y'); ?></time>
 			</p>
-			
 			<?php the_excerpt(); ?>
-			
 		</article>
-		
 	<?php endwhile; ?>
-	
 <?php else : ?>
-
 	<h2>Nothing Found</h2>
-	
 <?php endif; ?>
