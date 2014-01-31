ca3707a90ce7a9e3ef883b2faea2032e1fbeabef
Jan Reimers <me@janreimers.net>
2014-01-31 22:37:25 +0100

    cleanup

--- header.php
+++ header.php
@@ -1,12 +1,11 @@
 <?php 
 	$attachment_id = get_post_thumbnail_id();
 	$image_attributes = wp_get_attachment_image_src( $attachment_id, 'full' );
-?> 
-
+?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-  <title><?php wp_title( '-', true, 'right' ); bloginfo( 'name' ); ?></title>
+	<title><?php wp_title( '-', true, 'right' ); bloginfo( 'name' ); ?></title>
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<link href="<?php bloginfo('template_url'); ?>/images/favicon.ico" rel="shortcut icon" />
 	<meta name="viewport" content="width=device-width,initial-scale=1" />
@@ -14,31 +13,21 @@
 </head>
 
 <body <?php body_class(); ?>>
-
 	<?php if ( is_single() ): ?>
-	
-		<nav id="access" role="navigation">
-			<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
-		</nav>
-		
-		<header style="background-image: url(<?php echo $image_attributes[0]; ?>);">
-			<h1><?php the_title(); ?></h1>
-		</header>
-		
+	<nav id="access" role="navigation">
+		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
+	</nav>
+	<header style="background-image: url(<?php echo $image_attributes[0]; ?>);">
+		<h1><?php the_title(); ?></h1>
+	</header>
 	<?php else : ?>
-	
 	<header style="background-image: url('http://lorempixel.com/g/1600/400/nature/5/');">
 		<h1><a href="<?php echo home_url( '/' ); ?>" rel="home"><?php bloginfo('name'); ?></a><br/><small><?php bloginfo('description'); ?></small></h1>
 	</header>
-
-  	<nav id="access" role="navigation">
-	  	<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
-  	</nav>
-  	
-  	<?php endif; ?>
-		
-	
+	<nav id="access" role="navigation">
+		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
+	</nav>
+	<?php endif; ?>
 
 <div class="wrapper">
-
 	<main>
