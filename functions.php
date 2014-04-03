bb7cd5d2df28c077f001598a34d98e1494e6d312
Jan Reimers <me@janreimers.net>
2014-04-03 04:42:42 +0200

    added textdomain to navigation

--- functions.php
+++ functions.php
@@ -5,17 +5,17 @@ function resblogsive_theme_features() {
 
 	// remove generator-tag from <head>
 	remove_action('wp_head', 'wp_generator');
-	
+
 	// add automatic feed links to <head>
 	add_theme_support( 'automatic-feed-links' );
 
 	// add support for featured images
 	add_theme_support( 'post-thumbnails' );
-	
+
 	// add some style to the backend editor
 	add_editor_style( 'css/editor-style.css' );
 
-	// custom header images	
+	// custom header images
 	$header_args = array(
 		'default-image'			=> '%s/images/headers/blurry.jpg',
 		'width'					=> 1600,
@@ -28,21 +28,12 @@ function resblogsive_theme_features() {
 		'uploads'				=> true,
 	);
 	add_theme_support( 'custom-header', $header_args );
-	
+
 	// register navigation menu
 	register_nav_menus( array(
-		'main_nav' => __( 'Main Navigation' ),
+		'primary' => __( 'Primary Navigation', 'resblogsive' ),
 	));
-	
-	// remove <p> tag from images
-	// snippet from http://css-tricks.com/snippets/wordpress/remove-paragraph-tags-from-around-images/
-	function filter_ptags_on_images($content){
-		return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
-		return preg_replace('/<p>\s*(<a .*>)?\s*(<blockquote .* \/blockquote>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
-		return preg_replace('/<p>\s*(<a .*>)?\s*(<code .* \/code>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
-	}
-	add_filter('the_content', 'filter_ptags_on_images');
-	
+
 }
 
 // hook into 'after_setup_theme' action
