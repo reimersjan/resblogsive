ff071ac92df4ce197a06dbac523cb64d1470e599
Jan Reimers <me@jan.rs>
2015-02-12 19:19:14 +0100

    changed WordPress link to https

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 </div> <!-- .wrapper -->
 
 <footer class="footer">
-	<p><?php _e('powered by', 'resblogsive'); ?> <a target="_blank" href="http://wordpress.org">WordPress</a> <?php _e('and', 'resblogsive'); ?> <a target="_blank" href="<?php echo wp_get_theme()->get( 'ThemeURI' ); ?>"><?php echo wp_get_theme()->get( 'Name' ); ?></a> <?php echo wp_get_theme()->get( 'Version' ); ?></p>
+	<p><?php _e('powered by', 'resblogsive'); ?> <a target="_blank" href="https://wordpress.org">WordPress</a> <?php _e('and', 'resblogsive'); ?> <a target="_blank" href="<?php echo wp_get_theme()->get( 'ThemeURI' ); ?>"><?php echo wp_get_theme()->get( 'Name' ); ?></a> <?php echo wp_get_theme()->get( 'Version' ); ?></p>
 </footer>
 
 <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
