b78ea7740edd3e7e75d6f978509bd45d303aeea2
Jan Reimers <me@jan.rs>
2015-06-12 14:03:26 +0200

    temporary header fix

--- header.php
+++ header.php
@@ -13,29 +13,16 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<?php if (is_single()): ?>
+
 	<button class="nav-trigger genericon genericon-menu"></button>
 	<button class="nav-close genericon genericon-close-alt"></button>
 	<nav class="access" role="navigation">
 		<?php wp_nav_menu(array('container_class' => 'menu-header', 'theme_location' => 'primary')); ?>
 	</nav>
-	<?php if (has_post_thumbnail()): ?>
-		<header style="background-image: url(<?php echo $image_attributes[0]; ?>);">
-	<?php else : ?>
-		<header style="background-image: url(<?php header_image(); ?>);">
-	<?php endif; ?>
+  <header>
 		<h1><?php the_title(); ?></h1>
 	</header>
-	<?php else : ?>
-	<header style="background-image: url(<?php header_image(); ?>);">
-		<h1><a href="<?php echo home_url('/'); ?>" rel="home"><?php bloginfo('name'); ?></a><br/><small><?php bloginfo('description'); ?></small></h1>
-	</header>
-	<button class="nav-trigger genericon genericon-menu"></button>
-	<button class="nav-close genericon genericon-close-alt"></button>
-	<nav class="access" role="navigation">
-		<?php wp_nav_menu(array('container_class' => 'menu-header', 'theme_location' => 'primary')); ?>
-	</nav>
-	<?php endif; ?>
+
 
 <div class="wrapper">
 	<main>
