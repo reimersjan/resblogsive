695e13e5f1160440a3eff959feef2fc37aa7af1d
Jan Reimers <me@jan.rs>
2015-06-14 16:01:12 +0200

    gulp-watch works now

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
-var gulp = require('gulp');
-var concatCss = require('gulp-concat-css');
+var gulp = require('gulp'),
+    concatCss = require('gulp-concat-css');
 
-gulp.task('default', function () {
+gulp.task('concat', function () {
   return gulp.src('css-dev/main.css')
     .pipe(concatCss("style.css"))
     .pipe(gulp.dest(''));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('css-dev/*.css', ['concat']);
+});
+
+gulp.task('default', ['watch', 'concat']);
