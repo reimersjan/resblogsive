35156d096b18deaa06e3ff9d1397fd15921c9266
Jan Reimers <me@jan.rs>
2015-06-14 18:12:23 +0200

    fix title duplication

--- page.php
+++ page.php
@@ -3,7 +3,6 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<h2><?php the_title(); ?></h2>
 		<div class="entry">
 			<?php the_content(); ?>
 		</div>
