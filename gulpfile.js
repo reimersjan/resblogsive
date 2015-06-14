b9c10af25dfbfcbbd69a74daa31a67c2416b299a
Jan Reimers <me@jan.rs>
2015-06-14 17:05:47 +0200

    move css-dev to assets/css

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp'),
     concatCss = require('gulp-concat-css');
 
 gulp.task('concat', function () {
-  return gulp.src('css-dev/main.css')
+  return gulp.src('assets/css/main.css')
     .pipe(concatCss("style.css"))
     .pipe(gulp.dest(''));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('css-dev/*.css', ['concat']);
+  gulp.watch('assets/**/*.css', ['concat']);
 });
 
 gulp.task('default', ['watch', 'concat']);
